13064d31a0ee094859511f13d390e81acc21f606
sarahharding1 <G00374825@gmit.ie>
2022-08-23 22:45:56 +0100

    Changed the anchor to link, badge next to the cart, local storage and addToCart acton

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useLocation, useParams } from "react-router-dom";
+import { addToCart } from "../actions/cartActions";
 
- export default function CartScreen(props) {
-    const params = useParams();
-    const productId = params.id;
-    const {search} =useLocation();
-    const qtyInUrl = new URLSearchParams(search).get('qty');
-    const qty = qtyInUrl?Number(qtyInUrl):1;
+export default function CartScreen(props) {
+  const params = useParams();
+  const productId = params.id;
+  const { search } = useLocation();
+  const qtyInUrl = new URLSearchParams(search).get("qty");
+  const qty = qtyInUrl ? Number(qtyInUrl) : 1;
 
-    return (
-        <div>
-          <h1>Cart Screen</h1>
-          <p>
-            ADD TO CART : ProductID: {productId} Qty: {qty}
-          </p>
-        </div>
-      );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
     }
+  }, []);
+  return (
+    <div>
+      <h1>Cart Screen</h1>
+      <p>
+        ADD TO CART : ProductID: {productId} Qty: {qty}
+      </p>
+    </div>
+  );
+}
