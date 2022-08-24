4202b74ac2fe7cb7d6f371ff994b4c19c96cb1f6
sarahharding1 <G00374825@gmit.ie>
2022-08-25 00:57:42 +0100

    Created sample users in MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+    users: [
+      {
+        name:'Sarah',
+        email:'sarahannharding18@gmail.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: true,
+      },
+      {
+        name:'JohnDoe',
+        email:'user@example.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: false,
+      }
+    ],
     products: [
       {
         _id: '1',
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
  const app = express();
+ // eslint-disable-next-line no-undef
+ mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://admin:admin@cluster0.o2riywz.mongodb.net/?retryWrites=true&w=majority',
+ {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+ 
+ });
 
  app.get('/api/products/:id', (req, res) => {
   const product = data.products.find( x => x._id === req.params.id);
@@ -19,6 +28,13 @@ import data from './data.js';
  app.get('/', (req, res) => {
    res.send('Server is ready');
  });
+
+ app.use((err, req, res, next) =>{
+  res.status(500).send({message: err.message});
+ });
+
+ app.use('/api/users', userRouter);
+ // eslint-disable-next-line no-undef
  const port = process.env.PORT || 5000;
  app.listen(port, () => {
    console.log(`Serve at http://localhost:${port}`);
