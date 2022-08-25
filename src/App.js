68fa9f150f7e5b4dd9594a261a9be01a843d5f73
sarahharding1 <G00374825@gmit.ie>
2022-08-25 16:52:32 +0100

    Implemented Signin Screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
 
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
         <Route path="/cart/:id?" component={ CartScreen }></Route>
         <Route path="/product/:id" component={ProductScreen}></Route>
         <Route path="/" component={HomeScreen} exact></Route>
+        <Route path="/signin" component={ SigninScreen }></Route>
         
       </main>
       
