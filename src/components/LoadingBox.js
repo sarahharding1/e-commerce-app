874c6df3db7cce73d633fe13514dc83128010810
sarahharding1 <G00374825@gmit.ie>
2022-08-25 23:30:57 +0100

    Created register screen and backend API

--- src/components/LoadingBox.js
+++ src/components/LoadingBox.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function LoadingBox(){
-    return(
-        <div>
-            <i className="fa fa-spinner fa-spin"></i> Loading...
-        </div>
-    )
+export default function LoadingBox() {
+  return (
+    <div className="loading">
+      <i className="fa fa-spinner fa-spin"></i> Loading...
+    </div>
+  );
 }
