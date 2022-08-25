b4ce2eecbc2c3254ad9bca1ee4dab5d56f388991
sarahharding1 <G00374825@gmit.ie>
2022-08-25 23:30:21 +0100

    Created register screen and backend API

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -32,7 +32,26 @@ userRouter.post(
         return;
       }
     }
-    res.status(401).send({message: 'invalid email or password'});
+    res.status(401).send({ message: "invalid email or password" });
+  })
+);
+
+userRouter.post(
+  "/register",
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+      id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser),
+    });
   })
 );
 
