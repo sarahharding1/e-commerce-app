e3ffa4debd5a403d368e20ce3724abed52ae667e
sarahharding1 <G00374825@gmit.ie>
2022-08-26 13:22:06 +0100

    Created place order API

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config();
 
@@ -30,6 +31,7 @@ app.use((err, req, res, next) => {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- backend/utils.js
+++ backend/utils.js
@@ -12,3 +12,23 @@ export const generateToken = (user) => {
     expiresIn: '30d',
   });
 };
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+if(authorization){
+  const token=authorization.slice(7,authorization.length);
+  // eslint-disable-next-line no-undef
+  jwt.verify(token,process.env.JWT_SECRET||'somethingsecret',
+  (err,decode)=>{
+      if(err){
+          res.status(401).send({message:'invalid token'});
+      }else{
+          req.user=decode;
+          next();
+      }
+  }
+  );
+}
+else{
+  res.status('401').send({message:'No Token'});
+}
+};
