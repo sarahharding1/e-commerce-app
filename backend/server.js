f4eef5ad772060b7a1ad6259d5d5361a241c2c36
sarahharding1 <G00374825@gmit.ie>
2022-08-30 19:40:14 +0100

    Upload Product Image

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from 'path';
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import orderRouter from "./routers/orderRouter.js";
+import uploadRouter from './routers/uploadRouter.js';
 
 dotenv.config();
 
@@ -29,6 +31,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://admin:admin@cluster0.
 //   }
 // );
 
+
 app.get("/api/config/paypal", (req, res) => {
   // eslint-disable-next-line no-undef
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
@@ -38,15 +41,18 @@ app.get("/", (req, res) => {
 });
 
 
-
+app.use('/api/uploads', uploadRouter);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
+const __dirname = path.resolve();
+ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
+
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
