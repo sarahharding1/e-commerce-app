edbbd08cc9b6fe780230083bc97b12eebc47b6c0
sarahharding1 <G00374825@gmit.ie>
2022-08-30 20:06:54 +0100

    Delete Product

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
 import Product from "../models/productModel.js";
-import { isAdmin, isAuth } from '../utils.js';
+import { isAdmin, isAuth } from "../utils.js";
 
 const productRouter = express.Router();
 
@@ -36,27 +36,27 @@ productRouter.get(
 );
 
 productRouter.post(
-  '/',
+  "/",
   isAuth,
   isAdmin,
   expressAsyncHandler(async (req, res) => {
     const product = new Product({
-      name: 'sample name ' + Date.now(),
-      image: '/images/p1.jpg',
+      name: "sample name " + Date.now(),
+      image: "/images/p1.jpg",
       price: 0,
-      category: 'sample category',
-      brand: 'sample brand',
+      category: "sample category",
+      brand: "sample brand",
       countInStock: 0,
       rating: 0,
       numReviews: 0,
-      description: 'sample description',
+      description: "sample description",
     });
     const createdProduct = await product.save();
-    res.send({ message: 'Product Created', product: createdProduct });
+    res.send({ message: "Product Created", product: createdProduct });
   })
 );
 productRouter.put(
-  '/:id',
+  "/:id",
   isAuth,
   isAdmin,
   expressAsyncHandler(async (req, res) => {
@@ -71,9 +71,29 @@ productRouter.put(
       product.countInStock = req.body.countInStock;
       product.description = req.body.description;
       const updatedProduct = await product.save();
-      res.send({ message: 'Product Updated', product: updatedProduct });
+      res.send({ message: "Product Updated", product: updatedProduct });
     } else {
-      res.status(404).send({ message: 'Product Not Found' });
+      res.status(404).send({ message: "Product Not Found" });
+    }
+  })
+);
+
+productRouter.delete(
+  "/:id",
+
+  isAuth,
+
+  isAdmin,
+
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      const deleteProduct = await product.remove();
+
+      res.send({ message: "Product Deleted", product: deleteProduct });
+    } else {
+      res.status(404).send({ message: "Product Not Found" });
     }
   })
 );
