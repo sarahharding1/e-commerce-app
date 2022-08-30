a1b03e607f6e2506d171d6643ac0a841b812aed4
sarahharding1 <G00374825@gmit.ie>
2022-08-30 19:10:23 +0100

    Built Product Edit Screen and Update Product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import SigninScreen from "./screens/SigninScreen";
+import ProductEditScreen from './screens/ProductEditScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -89,6 +90,11 @@ function App() {
           {/* The route will render the product and home screens */}
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen} exact></Route>
+          <Route
+             path="/product/:id/edit"
+             component={ProductEditScreen}
+             exact
+           ></Route>
           <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/shipping" component={ShippingAddressScreen}></Route>
